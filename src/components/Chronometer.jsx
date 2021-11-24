a030d7421dc48b92dbfbc399b145d336399f7360
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 18:54:38 -0300

    feat(Chronometer): implement clock functionality

--- src/components/Chronometer.jsx
+++ src/components/Chronometer.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { increment, reset } from '../actions';
+
 function Chronometer(props) {
-  const { hour, minute, second } = props.time;
+  const {
+    reset,
+    increment,
+    time: { second, minute, hour },
+  } = props;
+
+  useEffect(() => {
+    if (second === 60) {
+      reset('second');
+      increment('minute');
+    }
+  }, [increment, reset, second]);
+
+  useEffect(() => {
+    if (minute === 60) {
+      reset('minute');
+      increment('hour');
+    }
+  }, [increment, reset, minute]);
+
+  useEffect(() => {
+    if (hour > 23) {
+      reset('hour');
+    }
+  }, [reset, hour]);
 
   return (
     <section>
       <h2>
-        <span>{hour}</span>
-        <span>{minute}</span>
-        <span>{second}</span>
+        <span>{hour < 10 ? `0${hour}` : hour}</span>
+        <span>{minute < 10 ? `0${minute}` : minute}</span>
+        <span>{second < 10 ? `0${second}` : second}</span>
       </h2>
     </section>
   );
@@ -19,4 +45,9 @@ const mapStateToProps = (state) => ({
   time: state.time,
 });
 
-export default connect(mapStateToProps, null)(Chronometer);
+const mapDispatchToProps = (dispatch) => ({
+  reset: (timeType) => dispatch(reset(timeType)),
+  increment: (timeType) => dispatch(increment(timeType)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chronometer);
