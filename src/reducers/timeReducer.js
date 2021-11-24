48062a6f0c523b8238aaa82d261bc51faeea9f5c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 17:26:46 -0300

    feat(timeReducer): implement different types of time

--- src/reducers/timeReducer.js
+++ src/reducers/timeReducer.js
@@ -1,11 +1,15 @@
-const INITIAL_STATE = 0;
+const INITIAL_STATE = {
+  hour: 0,
+  minute: 0,
+  second: 0,
+};
 
 export default function timeReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return (state += 1);
+      return { ...state, [action.timeType]: (state[action.timeType] += 1) };
     case 'DECREMENT':
-      return (state -= 1);
+      return { ...state, [action.timeType]: (state[action.timeType] -= 1) };
     default:
       return state;
   }
