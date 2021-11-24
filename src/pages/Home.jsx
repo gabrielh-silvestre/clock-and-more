59ce075685e5d16ecb63fe7f0e532e7c6973a5d8
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 18:53:55 -0300

    feat(Home): implement start Chronemeter function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import Chronometer from '../components/Chronometer'
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Home() {
+import { increment } from '../actions';
+import Chronometer from '../components/Chronometer';
+import Button from '../components/Button';
+
+function Home(props) {
+  const { increment } = props;
+
+  function startRunning() {
+    setInterval(() => increment('second'), 1);
+  }
+
   return (
     <main>
       <Chronometer />
+      <Button handleClick={startRunning} btnText="Start" />
     </main>
-  )
+  );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: (timeType) => dispatch(increment(timeType)),
+});
+
+export default connect(null, mapDispatchToProps)(Home);
