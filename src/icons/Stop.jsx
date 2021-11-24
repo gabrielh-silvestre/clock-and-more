ce0d3fd06f84eb2a6cd7b499b1779794d64b2084
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 20:25:13 -0300

    feat(icons): create Stop icon

---
+++ src/icons/Stop.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Stop() {
+  return (
+    <svg
+      className="w-10 h-10"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z"
+      />
+    </svg>
+  );
+}
