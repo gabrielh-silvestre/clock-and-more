56c2ee09ec5d28d8dd0483333109b7f82d990af3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 17:24:39 -0300

    feat(pages): create page directory and Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Chronometer from '../components/Chronometer'
+
+export default function Home() {
+  return (
+    <main>
+      <Chronometer />
+    </main>
+  )
+}
