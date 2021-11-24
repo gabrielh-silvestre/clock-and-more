c7a16eba18331d0a6d88412031517422b259f16e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-24 20:25:55 -0300

    feat(Home): implement reset chronometer button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
-import { disableChronometer, enableChronometer, increment } from '../actions';
+import {
+  disableChronometer,
+  enableChronometer,
+  increment,
+  reset,
+} from '../actions';
 import Chronometer from '../components/Chronometer';
 import Button from '../components/Button';
 import Play from '../icons/Play';
 import Pause from '../icons/Pause';
+import Stop from '../icons/Stop';
 
 function Home(props) {
-  const { increment, enableChronometer, disableChronometer, isActive } = props;
+  const { reset, increment, enableChronometer, disableChronometer, isActive } =
+    props;
 
   const [intervalId, setIntervalId] = useState(null);
 
@@ -23,6 +30,14 @@ function Home(props) {
     }
   }
 
+  function resetRunning() {
+    clearInterval(intervalId);
+    disableChronometer();
+    reset('second');
+    reset('minute');
+    reset('hour');
+  }
+
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <Chronometer />
@@ -33,6 +48,12 @@ function Home(props) {
         <Pause />
         <Play />
       </Button>
+      <Button
+        handleClick={resetRunning}
+        className="flex bg-blue-900 px-6 py-1 rounded-xl text-xl text-gray-300 mt-4"
+      >
+        <Stop />
+      </Button>
     </main>
   );
 }
@@ -42,6 +63,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  reset: (timeType) => dispatch(reset(timeType)),
   increment: (timeType) => dispatch(increment(timeType)),
   enableChronometer: () => dispatch(enableChronometer()),
   disableChronometer: () => dispatch(disableChronometer()),
