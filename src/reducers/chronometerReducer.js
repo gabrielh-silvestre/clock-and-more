c06a2a9d40d8725b5e73125b38f416fdbba9e8c2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-25 13:05:56 -0300

    refactor(chronometerReducer): change initial state

--- src/reducers/chronometerReducer.js
+++ src/reducers/chronometerReducer.js
@@ -1,11 +1,14 @@
-const INITIAL_STATE = false;
+const INITIAL_STATE = {
+  isActive: false,
+  intervalId: null,
+};
 
 export default function chronometerReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'DISABLE':
-      return (state = false);
+      return { ...state, isActive: false, intervalId: null };
     case 'ENABLE':
-      return (state = true);
+      return { ...state, isActive: true, intervalId: action.interval };
     default:
       return state;
   }
