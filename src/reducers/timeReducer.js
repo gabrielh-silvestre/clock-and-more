7bd9990eb4e61de7f9a6ea8d4d01d9c6d6132028
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-25 15:43:58 -0300

    fix(timerActions): remove unnecessary param

--- src/reducers/timeReducer.js
+++ src/reducers/timeReducer.js
@@ -12,6 +12,8 @@ export default function timeReducer(state = INITIAL_STATE, action) {
       return { ...state, [action.timeType]: (state[action.timeType] -= 1) };
     case 'RESET':
       return { ...state, [action.timeType]: (state[action.timeType] = 0) };
+    case 'START_OVER':
+      return { ...state, [action.timeType]: (state[action.timeType] = 59) }
     default:
       return state;
   }
