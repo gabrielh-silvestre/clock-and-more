4fde4cf6e2b5c94cc4485bde91dd45e875107395
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-28 20:08:47 -0300

    refactor: change BrowserRoutes from Home to index

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import Chronometer from '../components/Chronometer';
 import Timer from '../components/Timer';
 
 export default function Home() {
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Chronometer} />
-          <Route path="/timer" component={Timer} />
-        </Switch>
-      </BrowserRouter>
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <Switch>
+        <Route exact path="/" component={Chronometer} />
+        <Route path="/timer" component={Timer} />
+      </Switch>
     </div>
   );
 }
