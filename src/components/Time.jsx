fd42b1953157af0fd994fec966049901b46c0e39
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-28 18:41:21 -0300

    feat(Time): change span to input to enable user set time

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { setTime } from '../actions';
 
-export default function Time(props) {
-  const { timeValue } = props;
+function Time(props) {
+  const { timeValue, timeType, setTime } = props;
+
+  function handleChange({ target: { value }}) {
+    setTime(timeType, Number(value));
+  }
 
   return (
-    <span className="mx-2 font-bold">{timeValue < 10 ? `0${timeValue}` : timeValue}</span>
+    <input
+      type="number"
+      name={timeType}
+      id={timeType}
+      value={timeValue < 10 ? `0${timeValue}` : timeValue}
+      onChange={ handleChange }
+    />
   );
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  setTime: (timeType, timeValue) => dispatch(setTime(timeType, timeValue)),
+});
+
 Time.propTypes = {
   timeValue: PropTypes.number.isRequired,
+  timeType: PropTypes.string.isRequired,
+  setTime: PropTypes.func.isRequired,
 };
+
+export default connect(null, mapDispatchToProps)(Time);
