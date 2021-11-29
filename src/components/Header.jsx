a488ad4b2e3734ff9691df89ba10d0c890767773
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-11-29 11:42:04 -0300

    feat(Header): disable navigation when clock is running

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export default function Header(props) {
-  const { tool } = props;
+function Header(props) {
+  const {
+    tool,
+    clockStats: { isActive },
+  } = props;
 
   return (
     <header className="w-full fixed top-0 flex justify-center text-gray-300">
       <div className="w-10/12 flex justify-between items-center text-2xl py-4 px-8">
         <section>
-          <h1>{ tool } (maybe more)</h1>
+          <h1 className="text-4xl">{tool} (maybe more)</h1>
         </section>
 
         <nav className="flex">
-          <Link className="mr-4" to="/">Chronometer</Link>
-          <Link to="/timer">Timer</Link>
+          {isActive ? (
+            <div className="mr-4 opacity-60">Chronometer</div>
+          ) : (
+            <Link className="mr-4" to="/">
+              Chronometer
+            </Link>
+          )}
+
+          {isActive ? (
+            <div className="opacity-60">Timer</div>
+          ) : (
+            <Link to="/timer">Timer</Link>
+          )}
         </nav>
       </div>
     </header>
   );
 }
+
+const mapStateToProps = (state) => ({
+  clockStats: state.chronometer,
+});
+
+export default connect(mapStateToProps, null)(Header);
